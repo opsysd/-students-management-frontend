5025bf3e0000ccc7e46a08030e13870972b5a3b4
opsysd <75551401+opsysd@users.noreply.github.com>
2021-12-19 19:08:50 +0200

    Update get_all.js

--- samples/js/get_all.js
+++ samples/js/get_all.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
     
+    // sample how to do Ajax request using jQuery
+    // required URL and type method
+    // success function handle the success response from the server backend
     $.ajax({
         url: "http://localhost:3000/",
         type: 'GET',
         dataType: 'json',
         success: function(response) {
+            // parse the JSON to Javascript objects
             let objects = JSON.parse(JSON.stringify(response));
 
+            // select tbody from table id result-table
             tbody = $("#result-table tbody");
             for(let obj of objects) {
                 let row = "<tr><td>"+obj['id']
@@ -17,6 +22,8 @@ $(document).ready(function() {
                 +"</td><td>"+obj['degree']
                 +"</td><td>"+obj['grade']
                 +"</td></tr>";
+                
+                // add the row to the tbody
                 tbody.append(row);
             }
         }
